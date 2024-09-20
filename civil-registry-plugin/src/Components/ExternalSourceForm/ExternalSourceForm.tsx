11535e4dbcb07482af101b5cebc36834ace8f9b9
Kai Vandivier <kpvandivier@gmail.com>
2024-09-20 09:31:19 +0200

    fix: use vanilla CSS in form

--- civil-registry-plugin/src/Components/ExternalSourceForm/ExternalSourceForm.tsx
+++ civil-registry-plugin/src/Components/ExternalSourceForm/ExternalSourceForm.tsx
@@ -1,53 +1,48 @@
 import React from 'react'
 import i18n from '@dhis2/d2-i18n'
-import { Button } from '@dhis2/ui'
-import { Field, Form, Formik } from 'formik'
+import { Button, Input, Label } from '@dhis2/ui'
 import { SetFieldValueProps } from '../../Plugin.types'
 import { useRoutedMutation } from '../../lib/useRoutedMutation'
+import classes from './ExternalSourceForm.module.css'
 
 type Props = {
     setFieldValue: (values: SetFieldValueProps) => void
 }
 
-// todo: DHIS2 components
 export const ExternalSourceForm = ({ setFieldValue }: Props) => {
     const [mutate, { loading }] = useRoutedMutation(setFieldValue)
+    const [patientId, setPatientId] = React.useState('')
 
     return (
-        <>
-            <Formik
-                initialValues={{ patientId: '' }}
-                onSubmit={({ patientId }) => {
-                    mutate({ registryId: patientId })
-                }}
-            >
-                <Form className={'flex px-3 mt-4 items-center'}>
-                    <label
-                        htmlFor={'patientId'}
-                        className={
-                            'basis-[200px] grow-0 shrink-0 pr-4 pl-1 text-[14px]'
-                        }
-                    >
-                        {i18n.t('Patient ID')}
-                    </label>
+        <div className={classes.formContainer}>
+            <div className={classes.labelContainer}>
+                <Label
+                    required
+                    htmlFor={'patientId'}
+                    className={classes.label}
+                >
+                    {i18n.t('Patient ID')}
+                </Label>
+            </div>
 
-                    <div
-                        className={'flex basis-[150px] grow gap-1 items-center'}
-                    >
-                        <Field
-                            name={'patientId'}
-                            className={
-                                'border text-[14px] border-[#a0adba] py-2 px-2.5 rounded grow'
-                            }
-                            placeholder={i18n.t('Enter patient ID')}
-                        />
+            <div className={classes.inputContainer}>
+                <Input
+                    name="patientId"
+                    value={patientId}
+                    onChange={({ value }) => setPatientId(value)}
+                    className="grow"
+                />
 
-                        <Button primary type={'submit'} loading={loading}>
-                            {i18n.t('Search')}
-                        </Button>
-                    </div>
-                </Form>
-            </Formik>
-        </>
+                <Button
+                    primary
+                    onClick={() => {
+                        mutate({ registryId: patientId })
+                    }}
+                    loading={loading}
+                >
+                    {i18n.t('Search')}
+                </Button>
+            </div>
+        </div>
     )
 }
