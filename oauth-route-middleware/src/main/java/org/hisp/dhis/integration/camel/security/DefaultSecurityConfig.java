ba78679058144aed61af5e47b48848d7df305e47
cjmamo <823038+cjmamo@users.noreply.github.com>
2024-09-24 18:34:58 +0200

    feat(DEVREL-9): flesh out civil registry gateway

--- oauth-route-middleware/src/main/java/org/hisp/dhis/integration/camel/security/DefaultSecurityConfig.java
+++ oauth-route-middleware/src/main/java/org/hisp/dhis/integration/camel/security/DefaultSecurityConfig.java
@@ -6,8 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -24,22 +26,39 @@ import java.util.function.Supplier;
 
 @Configuration
 public class DefaultSecurityConfig {
+
   @Bean
-  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http.authorizeHttpRequests(request -> request.requestMatchers("/api/**").permitAll()).
-            authorizeHttpRequests(request -> request.requestMatchers("/management/**", "/login", "/logout").authenticated()).
-            formLogin(Customizer.withDefaults()).headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)).
-            csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())).
-            httpBasic(Customizer.withDefaults()).
-            addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class).
-            build();
+  @Order(1)
+  protected SecurityFilterChain catchAllFilterChain(HttpSecurity http) throws Exception {
+    return http.securityMatcher("/**")
+        .authorizeHttpRequests(a -> a.requestMatchers("/api/find").permitAll())
+        .authorizeHttpRequests(a -> a.anyRequest().denyAll())
+        .csrf(AbstractHttpConfigurer::disable)
+        .build();
+  }
+
+  @Bean
+  @Order(0)
+  protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.securityMatcher("/management/**", "/login", "/logout", "/error")
+        .authorizeHttpRequests(a -> a.anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults())
+        .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+        .csrf(
+            csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+        .httpBasic(Customizer.withDefaults())
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+        .build();
   }
 
   static class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
     private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
 
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
+    public void handle(
+        HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
       /*
        * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
        * the CsrfToken when it is rendered in the response body.
@@ -59,8 +78,9 @@ public class DefaultSecurityConfig {
   static class CsrfCookieFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
       CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
       // Render the token value to a cookie by causing the deferred token to be loaded
       csrfToken.getToken();
