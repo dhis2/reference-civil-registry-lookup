8ade5ca8ad6379463d7c39377f029ff46fa4f704
cjmamo <823038+cjmamo@users.noreply.github.com>
2024-09-26 19:32:43 +0200

    feat: implement OAuth2 client credentials flow; support unhappy path
    refactor: delete unused class
    
    build: Dockerize oauth-route-middleware
    
    docs: add license headers

--- oauth-route-middleware/src/test/java/org/hisp/dhis/integration/camel/route/AbstractRouteFunctionalTestCase.java
+++ oauth-route-middleware/src/test/java/org/hisp/dhis/integration/camel/route/AbstractRouteFunctionalTestCase.java
@@ -1,5 +1,34 @@
+/*
+ *  Copyright (c) 2004-2024, University of Oslo
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *  Neither the name of the HISP project nor the names of its contributors may
+ *  be used to endorse or promote products derived from this software without
+ *  specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.hisp.dhis.integration.camel.route;
 
+import ca.uhn.fhir.context.FhirVersionEnum;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
@@ -16,7 +45,7 @@ import java.time.Duration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CamelSpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AbstractRouteFunctionalTestCase {
 
   @Container public static GenericContainer<?> HAPI_FHIR_CONTAINER;
@@ -24,6 +53,8 @@ public class AbstractRouteFunctionalTestCase {
   @Autowired protected CamelContext camelContext;
 
   @Autowired protected ProducerTemplate producerTemplate;
+
+  protected static IGenericClient FHIR_CLIENT;
 
   private static GenericContainer<?> newHapiFhirContainer() {
     return new GenericContainer<>(DockerImageName.parse("hapiproject/hapi:v7.4.0-tomcat"))
@@ -33,13 +64,16 @@ public class AbstractRouteFunctionalTestCase {
   }
 
   @BeforeAll
-  public static void beforeAll() throws Exception {
+  public static void beforeAll() {
     if (HAPI_FHIR_CONTAINER == null) {
       HAPI_FHIR_CONTAINER = newHapiFhirContainer();
       HAPI_FHIR_CONTAINER.start();
 
-      System.setProperty(
-          "fhir.server-url", String.format("http://localhost:%s/fhir", HAPI_FHIR_CONTAINER.getFirstMappedPort()));
+      String fhirServerUrl =
+          String.format("http://localhost:%s/fhir", HAPI_FHIR_CONTAINER.getFirstMappedPort());
+      FHIR_CLIENT = FhirVersionEnum.R4.newContext().newRestfulGenericClient(fhirServerUrl);
+
+      System.setProperty("camel.component.fhir.server-url", fhirServerUrl);
     }
   }
 }
