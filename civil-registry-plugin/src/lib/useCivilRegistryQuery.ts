10eb56b52dcc0a66fcc56c69b9c47429c6bc5567
Kai Vandivier <kpvandivier@gmail.com>
2024-09-26 17:10:43 +0200

    chore: cleanup

--- civil-registry-plugin/src/lib/useCivilRegistryQuery.ts
+++ civil-registry-plugin/src/lib/useCivilRegistryQuery.ts
@@ -4,24 +4,6 @@ import i18n from '../locales'
 
 type Props = { setFieldValue: SetFieldValue; fieldsMetadata: FieldsMetadata }
 
-// todo: remove after testing
-const mockPerson = {
-    id: 'abcde12345',
-    firstName: 'History',
-    lastName: 'Museum',
-    sex: 'MALE',
-    dateOfBirth: '1876-09-01',
-    phone: '+998 71 239 17 79',
-    address: [
-        'Sharaf Rashidov Avenue 3',
-        '100029',
-        'Mirobod',
-        'Tashkent',
-        'Tashkent City',
-        'Uzbekistan',
-    ].join('\n'),
-}
-
 const mutation = {
     // todo: verify code
     resource: 'route/civil-registry/run',
@@ -57,22 +39,6 @@ export const useCivilRegistryQuery = ({
                     error: error.details.message || error.message,
                     nsSeparator: '`',
                 }),
-            })
-
-            // todo: remove after testing
-            // logic copied from above
-            // ---
-            // Take data returned from Route and set enrollment field values.
-            // Expects a flat object
-            Object.entries(mockPerson).forEach(([key, value]) => {
-                // Avoids setting values outside of plugin's configured fields
-                if (Object.hasOwn(fieldsMetadata, key)) {
-                    setFieldValue({ fieldId: key, value: value })
-                } else {
-                    console.warn(
-                        `Field ID "${key}" not found in configured fields; skipping value "${value}"`
-                    )
-                }
             })
         },
     })
