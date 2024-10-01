61b73a29d9ca8a9f0cf062682f7fec6c9b747c82
cjmamo <823038+cjmamo@users.noreply.github.com>
2024-10-01 10:48:58 +0200

    feat: implement OAuth2 client credentials flow; support unhappy path (#16)
    refactor: delete unused class
    
    build: Dockerize oauth-route-middleware
    
    docs: add license headers

--- oauth-route-middleware/src/main/java/org/hisp/dhis/integration/camel/Application.java
+++ oauth-route-middleware/src/main/java/org/hisp/dhis/integration/camel/Application.java
@@ -1,51 +1,40 @@
+/*
+ *  Copyright (c) 2004-2024, University of Oslo
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *  Neither the name of the HISP project nor the names of its contributors may
+ *  be used to endorse or promote products derived from this software without
+ *  specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.hisp.dhis.integration.camel;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.context.FhirVersionEnum;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-    protected static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
-    @Value("${fhir.server-url:}")
-    private String fhirServerUrl;
-
-    @Autowired
-    private ConfigurableApplicationContext applicationContext;
-
-    @Bean
-    public FhirContext fhirContext() {
-        return FhirVersionEnum.R4.newContext();
-    }
-
-    public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.run(args);
-    }
-
-    @Bean
-    public IGenericClient fhirClient(FhirContext fhirContext) {
-        if (!StringUtils.hasText(fhirServerUrl)) {
-            terminate("Missing FHIR Server URL. Are you sure that you set `fhir.server-url`?");
-        }
-        return fhirContext.newRestfulGenericClient(fhirServerUrl);
-    }
-
-
-    protected void terminate(String shutdownMessage) {
-        LOGGER.error("TERMINATING!!! {}", shutdownMessage);
-        applicationContext.close();
-        System.exit(1);
-    }
+  public static void main(String[] args) {
+    SpringApplication springApplication = new SpringApplication(Application.class);
+    springApplication.run(args);
+  }
 }
