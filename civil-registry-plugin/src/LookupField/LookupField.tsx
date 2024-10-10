b20d186bdef24972c63d920fd5fbcb33cd544e67
Kai Vandivier <kpvandivier@gmail.com>
2024-10-10 16:41:06 +0200

    feat: update parent ID field value on field change

--- civil-registry-plugin/src/LookupField/LookupField.tsx
+++ civil-registry-plugin/src/LookupField/LookupField.tsx
@@ -1,18 +1,41 @@
 import i18n from '@dhis2/d2-i18n'
 import { Button, Input, Label } from '@dhis2/ui'
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { useCivilRegistryQuery } from '../lib/useCivilRegistryQuery'
 import { FieldsMetadata, SetFieldValue } from '../Plugin.types'
 import classes from './LookupField.module.css'
 
-type Props = { setFieldValue: SetFieldValue; fieldsMetadata: FieldsMetadata, values: Record<string, any> }
+// ! NB: This is a little custom, and not so generic
+let idWarningIssued = false
+const idWarning =
+    "No field with a plugin alias `id` has been found; the value in this field won't automatically update the form value. Values returned from the civil registry still may, depending on the configured plugin aliases."
 
-export const LookupField = ({ setFieldValue, fieldsMetadata, values }: Props) => {
+type Props = {
+    setFieldValue: SetFieldValue
+    fieldsMetadata: FieldsMetadata
+    values: Record<string, any>
+}
+
+export const LookupField = ({
+    setFieldValue,
+    fieldsMetadata,
+    values,
+}: Props) => {
     const [query, { loading }] = useCivilRegistryQuery({
         setFieldValue,
         fieldsMetadata,
     })
-    const [patientId, setPatientId] = React.useState(values['id'] || '')
+    const [patientId, setPatientId] = useState(values['id'] || '')
+
+    const handleChange = useCallback(({ value }) => {
+        setPatientId(value)
+        if ('id' in fieldsMetadata) {
+            setFieldValue({ fieldId: 'id', value })
+        } else if (!idWarningIssued) {
+            console.warn(idWarning)
+            idWarningIssued = true
+        }
+    }, [])
 
     return (
         <div className={classes.fieldContainer}>
@@ -22,20 +45,24 @@ export const LookupField = ({ setFieldValue, fieldsMetadata, values }: Props) =>
                 </Label>
             </div>
 
-            <div className={classes.inputContainer}>
-                <Input
-                    name="patientId"
-                    value={patientId}
-                    onChange={({ value }) => setPatientId(value)}
-                    className={classes.input}
-                />
+            <div className={classes.input}>
+                <div className={classes.inputContainer}>
+                    <Input
+                        name="patientId"
+                        value={patientId}
+                        onChange={handleChange}
+                        className={classes.input}
+                    />
 
-                <Button
-                    onClick={() => query({ id: patientId })}
-                    loading={loading}
-                >
-                    {i18n.t('Search')}
-                </Button>
+                    <Button
+                        onClick={() => {
+                            query({ id: patientId })
+                        }}
+                        loading={loading}
+                    >
+                        {i18n.t('Search')}
+                    </Button>
+                </div>
             </div>
         </div>
     )
