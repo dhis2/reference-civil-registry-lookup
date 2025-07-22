a207bfce8e5c9965764131ab44ea7a2de298bd5e
Kai Vandivier <kpvandivier@gmail.com>
2025-07-23 00:02:52 +0200

    refactor: tidying

--- civil-registry-plugin/src/lib/useCivilRegistryQuery.ts
+++ civil-registry-plugin/src/lib/useCivilRegistryQuery.ts
@@ -1,8 +1,12 @@
 import { useDataMutation } from '@dhis2/app-runtime'
 import { FieldsMetadata, SetFieldValue } from '../Plugin.types'
-import { default as jsonata } from 'jsonata';
+import jsonata from 'jsonata'
 
-type Props = { setFieldValue: SetFieldValue; fieldsMetadata: FieldsMetadata }
+type Props = {
+    setFieldValue: SetFieldValue
+    fieldsMetadata: FieldsMetadata
+    personMap?: { escapedScript: string }
+}
 
 const mutation = {
     resource: 'routes/civil-registry/run',
@@ -13,29 +17,27 @@ const mutation = {
 export const useCivilRegistryQuery = ({
     setFieldValue,
     fieldsMetadata,
-    personMap
+    personMap,
 }: Props) => {
     return useDataMutation(mutation as any, {
-        onComplete: (fhirPerson) => {
-            (async () => {
-                const lookupPerson = await jsonata(
-                    personMap?.escapedScript
-                ).evaluate(fhirPerson)
+        onComplete: async (fhirPerson) => {
+            const lookupPerson = await jsonata(
+                personMap?.escapedScript
+            ).evaluate(fhirPerson)
 
-               // Take data returned from Route and set enrollment field values.
-               // Expects a flat object, and for keys and values to match the
-               // plugin's configured fields
-                Object.entries(lookupPerson).forEach(([key, value]) => {
-                        // Avoids setting values outside of plugin's configured fields
-                        if (Object.hasOwn(fieldsMetadata, key)) {
-                            setFieldValue({ fieldId: key, value: value })
-                        } else {
-                            console.warn(
-                                `Field ID "${key}" not found in configured fields; skipping value ${value}`
-                            )
-                        }
-                })
-            })()
+            // Take data returned from Route and set enrollment field values.
+            // Expects a flat object, and for keys and values to match the
+            // plugin's configured fields
+            Object.entries(lookupPerson).forEach(([key, value]) => {
+                // Avoids setting values outside of plugin's configured fields
+                if (Object.hasOwn(fieldsMetadata, key)) {
+                    setFieldValue({ fieldId: key, value: value })
+                } else {
+                    console.warn(
+                        `Field ID "${key}" not found in configured fields; skipping value ${value}`
+                    )
+                }
+            })
         },
         onError: (error) => {
             console.error(error.details || error)
